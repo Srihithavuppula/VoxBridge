9d4d0cbabfe63f4fb3eda2f59d64ce48e4d3e8ae
Student Apnacollege <student@apnacollege>
2026-04-05 11:56:01 +0530

    amain .jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // frontend/src/main.jsx
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./app"
+import App from "./App"
 import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
